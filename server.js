7a93ea5ce5c99da8e4433535abb9bef9f7a0e931
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-04-27 14:46:57 -0400

    added connection to db and queried table

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const res = require("express/lib/response");
+const mysql = require("mysql2");
 // port
 const PORT = process.env.PORT || 3001;
 
@@ -10,12 +11,29 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to db
+const db = mysql.createConnection(
+  {
+    host: "localHost",
+    //username
+    user: "root",
+    //password
+    password: "rootroot",
+    database: "election",
+  },
+  console.log("Connected to the election database!")
+);
+
 //test route
 // app.get("/", (req, res) => {
 //   res.json({
 //     message: "hello world",
 //   });
 // });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 app.use((req, res) => {
   res.status(404).end();
 });
